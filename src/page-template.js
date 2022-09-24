8fe7f118a94c4eb319f40b1d9fab95e2f20c4c65
connork96 <connorkelly96@yahoo.com>
2022-09-24 16:59:29 -0400

    got the application working

--- src/page-template.js
+++ src/page-template.js
@@ -15,7 +15,7 @@ module.exports = team => {
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 jumbotron mb-3 team-heading">
-                    <h1 class="text-center">The Dream Team</h1>
+                    <h1 class="text-center">The Geek Squad</h1>
                 </div>
             </div>
         </div>
